2d116ac587dbf0f29ab5a932ed17ee43099e4869
Czaja <czajkamateusz999@gmail.com>
2023-11-14 16:48:02 +0100

    camunda modeler basic process, java project

---
+++ src/main/java/org/bp/WarehousesApplication.java
@@ -0,0 +1,9 @@
+package org.bp;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Warehouses Application")
+public class WarehousesApplication extends ServletProcessApplication {
+
+}
